309f6a78a5e75fd90a9d1982d183359889cc85d1
Rodrigo Silva <rodrigo@rodrigosilva.user.f1s.me>
2018-08-31 17:27:04 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = require('./config/server');
+
+var rotaNoticias = require('./app/routes/noticias')
+rotaNoticias(app)
+
+
+app.listen(3000,function(){
+	console.log('Servidor ON')
+})
+
+
---
+++ noticias.js
@@ -0,0 +1,13 @@
+var http = require('http');
+
+var server = http.createServer( function(req,res){
+	var categoria = req.url
+	if(categoria == '/tecnologia'){
+		res.end("<html><body>Acessando página de Técnologia por req.URL</body></html>")
+	}else{
+		res.end("<html><body>Portal noticias</body></html>")
+		//res.end(categoria)
+	}
+});
+
+server.listen(3000);
