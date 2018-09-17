35e6a3e184b6a799aa595939f6cd999594816522
Rodrigo Silva <rodrigo@rodrigosilva.user.f1s.me>
2018-09-17 15:17:51 -0300

    atualização de aulas

--- noticias.js
+++ noticias.js
@@ -1,13 +1,14 @@
 var http = require('http');
 
-var server = http.createServer( function(req,res){
-	var categoria = req.url
-	if(categoria == '/tecnologia'){
-		res.end("<html><body>Acessando página de Técnologia por req.URL</body></html>")
-	}else{
-		res.end("<html><body>Portal noticias</body></html>")
-		//res.end(categoria)
-	}
+var server = http.createServer(function(req, res) {
+    var categoria = req.url
+    if (categoria == '/tecnologia') {
+        res.end("<html><body>Acessando página de Técnologia por req.URL</body></html>")
+    } else {
+
+        res.end("<html><body>Portal noticias</body></html>")
+            //res.end(categoria)
+    }
 });
 
 server.listen(3000);
