599300af693cc3c39b583e718946b7cf6ac90989
Rodrigo Silva <rodrigo@rodrigosilva.user.f1s.me>
2018-09-19 16:36:58 -0300

    atualizacao de modulos

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,7 @@
 module.exports = function(app) {
     app.get('/', function(req, res) {
+        // res.render("header")
         res.render("home/index")
+            // res.render("footer")
     })
 }
