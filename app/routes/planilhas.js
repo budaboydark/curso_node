5812cca76ebccf8f77a09dc188390dfc51b8b0a2
Rodrigo Silva <rodrigo@rodrigosilva.user.f1s.me>
2018-10-02 14:47:31 -0300

    tentativa de colocar querys em variáveis no resultado

--- app/routes/planilhas.js
+++ app/routes/planilhas.js
@@ -1,11 +1,32 @@
 var dbConnection = require('../../config/dbConnection')
 module.exports = function(app) {
     var connection = dbConnection()
-    app.get('/planilhas', function(req, res) {
-        res.render('planilhas/planilha')
+    app.get('/planilhas/:id?', function(req, res) {
+        // console.log(req.params.id)
+        // var id = req.params.id
 
-        // connection.query("SELECT * FROM contas_pagar", function (erro, result) {
-        //     res.render('contas/contas', { contas: result })
-        // })
+
+        var f = connection.query("SELECT * FROM contas_pagar", function(erro, result) {
+            console.log(result)
+        })
+
+        var data = {
+            dados: {
+                [1]: { mes: "janeiro" },
+                [2]: { mes: "fevereiro" },
+                [3]: { mes: "março" },
+                [4]: { mes: "abril" },
+                [5]: { mes: "maio" },
+                [6]: { mes: "junho" },
+                [7]: { mes: "julho" },
+                [8]: { mes: "agosto" },
+                [9]: { mes: "setembro" },
+                [10]: { mes: "outubro" },
+                [11]: { mes: "novembro" },
+                [12]: { mes: "dezembro" }
+            }
+
+        }
+        res.render('planilhas/planilha', data)
     })
 }
