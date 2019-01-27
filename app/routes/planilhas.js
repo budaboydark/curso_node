488d4a010e07a601a925e20d95a35ae898ae84b1
budaboydark <blueinforbs@gmail.com>
2019-01-27 09:48:16 -0200

    automatizando planilha de contas em tabs

--- app/routes/planilhas.js
+++ app/routes/planilhas.js
@@ -1,20 +1,20 @@
 module.exports = function(app) {
     app.get('/planilhas/:id?', (req, res) => {
         var data = {
-            dados: {
-                [1]: { mes: "janeiro" },
-                [2]: { mes: "fevereiro" },
-                [3]: { mes: "março" },
-                [4]: { mes: "abril" },
-                [5]: { mes: "maio" },
-                [6]: { mes: "junho" },
-                [7]: { mes: "julho" },
-                [8]: { mes: "agosto" },
-                [9]: { mes: "setembro" },
-                [10]: { mes: "outubro" },
-                [11]: { mes: "novembro" },
-                [12]: { mes: "dezembro" }
-            }
+            dados: [
+                { mes: "janeiro" },
+                { mes: "fevereiro" },
+                { mes: "março" },
+                { mes: "abril" },
+                { mes: "maio" },
+                { mes: "junho" },
+                { mes: "julho" },
+                { mes: "agosto" },
+                { mes: "setembro" },
+                { mes: "outubro" },
+                { mes: "novembro" },
+                { mes: "dezembro" }
+            ]
         }
         res.render('planilhas/planilha', data)
     })
